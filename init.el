4118920fb682ce2755ca96bb5ab3ef737318f90a
Shuto Shikama <@gmail.com>
2022-02-12 16:39:46 +0900

    Better editing

--- init.el
+++ init.el
@@ -562,6 +562,10 @@ configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
   (spacemacs/toggle-transparency)
+  (blink-cursor-mode -1)
+  (setq-default truncate-lines t)
+  (setq-default truncate-partial-width-windows t)
+  (setq evil-emacs-state-cursor '("SkyBlue2" bar))
 )
 
 
